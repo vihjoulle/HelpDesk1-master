fc4987b79c751f8138b25ddecc4ad656620d3527
VihJoulle <victordeghandi@gmail.com>
2024-03-04 13:48:48 -0300

    Commit do dia 04/03

---
+++ HelpDesk1-master/src/main/java/com/victor/HelpDesk/Config/DevConfig.java
@@ -0,0 +1,26 @@
+package com.victor.HelpDesk.Config;
+
+import com.victor.HelpDesk.Service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("Dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+    @Value("${spring.jpa.hibernate.ddl-auto")
+    private String value;
+    @Bean
+    public boolean instanciaDB(){
+
+        if(value.equals("create")){
+            this.dbService.instanciaDB();
+        }
+        return false;
+    }
+}
